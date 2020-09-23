a799bf21a2860d47a25531d5254d0afeb8ac9467
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-23 09:33:36 -0600

    Add add_ingredient and ingredients methods

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -3,6 +3,14 @@ class Recipe
 
   def initialize(name)
     @name                 = name
-    @ingredients_required = {}
+    @ingredients_required = Hash.new(0)
+  end
+
+  def add_ingredient(ingredient, qty)
+    @ingredients_required[ingredient] += qty
+  end
+
+  def ingredients
+    @ingredients_required.keys
   end
 end
