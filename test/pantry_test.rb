9b805880b459f8db1ccc97df5ec9848291ee0071
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-23 09:21:12 -0600

    add assert for ingredient2's stock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -25,5 +25,9 @@ class PantryTest < Minitest::Test
     @pantry.restock(@ingredient1, 10)
 
     assert_equal 15, @pantry.stock_check(@ingredient1)
+
+    @pantry.restock(@ingredient2, 7)
+
+    assert_equal 7, @pantry.stock_check(@ingredient2)
   end
 end
