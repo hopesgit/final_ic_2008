ddc1a9b82217004852ae108b93e187376cc37e23
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-23 09:31:59 -0600

    Add test for ingredient-related methods in recipe

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -18,4 +18,14 @@ class RecipeTest < Minitest::Test
     assert_equal "Mac and Cheese", @recipe1.name
     assert_instance_of Hash, @recipe1.ingredients_required
   end
+
+  def test_it_can_hold_required_ingredients
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    expected = {@ingredient1=>6, @ingredient2=>8}
+
+    assert_equal expected, @recipe1.ingredients_required
+    assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients
+  end
 end
