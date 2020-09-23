38a9a7773e0c27a8b4e49879ebec01b58959e11d
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-23 09:37:28 -0600

    Add cookbook test

---
+++ test/cookbook_test.rb
@@ -0,0 +1,19 @@
+require "./lib/ingredient"
+require "./lib/recipe"
+require "./lib/cookbook"
+require "minitest/autorun"
+require "minites/pride"
+
+class CookBookTest < Minitest::Test
+  def setup
+    @cookbook = CookBook.new
+    @recipe2 = Recipe.new("Cheese Burger")
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1 = Recipe.new("Mac and Cheese")
+  end
+
+  def test_it_is_cookbook
+    assert_instance_of CookBook, @cookbook
+  end
+end
