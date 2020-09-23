f4a827c22ce13c917c00efed9a2f3ca448508119
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-23 10:37:45 -0600

    Add test for CookBook#summary method

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -45,4 +45,17 @@ class CookBookTest < Minitest::Test
   def test_it_knows_date_it_was_created
     assert_equal "09-23-2020", @cookbook.date
   end
+
+  def test_it_can_get_summary_of_recipes
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 1})
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 100)
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    expected = [{:name=>"Mac and Cheese", :details=>{:ingredients=>[{:ingredient=>"Macaroni", :amount=>"8 oz"}, {:ingredient=>"Cheese", :amount=>"2 C"}], :total_calories=>440}}, {:name=>"Burger", :details=>{:ingredients=>[{:ingredient=>"Ground Beef", :amount=>"4 oz"}, {:ingredient=>"Bun", :amount=>"100 g"}], :total_calories=>500}}]
+
+    assert_equal expected, @cookbook.summary
+  end
 end
